786b377d79b04b214791ed9605215cd533700b29
Zuriel Zavala <zzavala2@hotmail.com>
2026-01-23 17:53:28 -0600

    added central widget group

--- entry.py
+++ entry.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QPushButton
 import sys
 import backlogObj
+import backlogEdit
 
 
 # Entry point for the application
@@ -9,12 +10,15 @@ import backlogObj
 class EntryWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        
+        #main group for adding other widgets later
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
         self.setWindowTitle("Backlog App")
         self.setGeometry(100, 100, 300, 200)
         
         button = QPushButton("Exit")
         button.pressed.connect(self.close)
-        self.setCentralWidget(button)
         self.show()
 app = QApplication(sys.argv)
 window = EntryWindow()
