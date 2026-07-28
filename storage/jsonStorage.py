0fcc03c4c3a7f15a8386016d2b52b72b4dc4f820
Zuriel Zavala <ozzavala1011@gmail.com>
2026-07-27 19:58:18 -0500

    last min work, got correct path saving for JSON

--- storage/jsonStorage.py
+++ storage/jsonStorage.py
@@ -1,12 +1,12 @@
 import json
 import os
 
-
+#save JSON file to data/JSON folder, if the folder does not exist, create it
 class jsonStorage:
     def save(self, data, filename):
-        desired_dir = "<c>:/dev/backlogApp/data"
-        full_path = os.path.join(desired_dir, filename)
-        with open(filename, 'w') as f:
+        desired_dir = "data/JSON"
+        os.makedirs(desired_dir, exist_ok=True)
+        with open(os.path.join(desired_dir, filename), 'w') as f:
             json.dump(data, f, indent=4)
     def load(self, filename):
         with open(filename, 'r') as f:
