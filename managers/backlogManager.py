d8c8b227641d94629f40d1c22f5bfbcdb62c7e31
Zuriel Zavala <ozzavala1011@gmail.com>
2026-08-06 01:10:13 -0500

    revising backlog manager + obj class'

--- managers/backlogManager.py
+++ managers/backlogManager.py
@@ -1,23 +1,25 @@
 import storage.jsonStorage as jsonStorage
-import models.backlogObj as backlogObj
+from models.backlogObj import backlogObj
+from dataclasses import asdict
 class backlogManager():
     def __init__(self):
-        self._entries = []
+        #where each entry is a backlogObj instance
+        self._entries: list[backlogObj] = []
         
-    def add(self, entry):
+    def add(self, entry: backlogObj) -> None:
         self._entries.append(entry)
         
-    def remove(self, entry):
+    def remove(self, entry: backlogObj):
         if isinstance(entry, str):
             self._entries = [e for e in self._entries if e.title != entry]
         else:
             self._entries = [e for e in self._entries if e != entry]
 
-    def find(self, title):
+    def find(self, title: str) -> backlogObj | None:
         for entry in self._entries:
             if entry.title == title:
-                return entry.copy()
-            
+                return entry
+        return None
     
     def edit(self, title, new_title=None, new_description=None, new_status=None, new_thumbnail=None, new_rating=None):
         entry = self.find(title)
@@ -36,9 +38,18 @@ class backlogManager():
         return False
     
     def display_all_entries(self):
-        return [entry.copy() for entry in self._entries]
+        return self._entries.copy()
     
     def save_to_file(self, filename):
         storage = jsonStorage.jsonStorage()
-        data = [entry.__dict__ for entry in self._entries]
+        data = [asdict(entry) for entry in self._entries]
+        
         storage.save(data, filename)
+        
+    def load_from_file(self, filename):
+        storage = jsonStorage.jsonStorage()
+        data = storage.load(filename)
+        
+        self._entries = [backlogObj.backlogObj(**entry) for entry in data]
+    def clear_entries(self):
+        self._entries.clear()
